1ffc92e8d2660a54eb149159f3d5db995c832534
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-14 14:35:02 -0400

    all tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,26 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function getTasks() {
+    return db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select(
+            't.task_id', 
+            't.task_description', 
+            't.task_notes', 
+            't.task_completed', 
+            'p.project_name', 
+            'p.project_description'
+        )
+
+}
+
+async function createTask(task) {
+    const id = await db('tasks').insert(task)
+    return db('tasks').where('task_id', id).first()
+}
+
+module.exports = {
+    getTasks,
+    createTask
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,33 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+const Tasks = require('./model')
+
+function convertBoolean(task) {
+    const converted = task.map(p => {
+        p.project_completed === 0 ? p.project_completed = false : p.project_completed = true
+        return p
+    })
+    return converted
+}
+
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await Tasks.getTasks()
+        tasks.map(task => task.task_completed === 0 ? task.task_completed = false : task.task_completed = true)
+        res.status(200).json(tasks)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newTask = await Tasks.createTask(req.body)
+        newTask.task_completed === 0 ? newTask.task_completed = false : newTask.task_completed = true
+        res.status(201).json(newTask)
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
